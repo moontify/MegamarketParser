b1f099fc83129417dccf89760ec32af6298212e3
Ilya Mochov <115039016+moontify@users.noreply.github.com>
2024-09-30 00:22:52 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher
+import main
+TELEGRAM_TOKEN = "Ваш токен бота"
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+# Зачем что-то делать если можно забить хуй))
+# Реализация есть в мейне
